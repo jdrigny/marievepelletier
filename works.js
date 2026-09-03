a1b28e9dab9ec87c798f8378d491667ec1cd7c22
jdrigny <j.drigny@gmail.com>
2026-09-03 10:25:02 +0200

    Update works.js

--- works.js
+++ works.js
@@ -32,7 +32,8 @@ const WORKS = [
   {src:`images/33_Vue_datelier_photo_numerique.webp`,alt:`Vue d'atelier`,title:`Vue d'atelier`,fr:`2026, photographie numérique`,en:`2026, digital photograph`,year:2026},
   {src:`images/34_Apparition_disparition.webp`,alt:`Apparition/disparition`,title:`Apparition/disparition`,fr:`workshop réalisé avec l'artiste Pierre Tatu, ésam Caen/Cherbourg, hiver 2017`,en:`workshop with artist Pierre Tatu, ésam Caen/Cherbourg, winter 2017`,year:2017},
   {src:`images/35_Its_over.webp`,alt:`It's over`,title:`It's over`,fr:`2023, huile sur toile, 30 × 30 cm`,en:`2023, oil on canvas, 30 × 30 cm`,year:2023},
-  {src:`images/cyanocolo.png`,alt:`Sans titre`,title:`Sans titre`,fr:`2026, cyanotype colorisé à la main, 40 × 30 cm`,en:`2026, hand-colored cyanotype, 40 × 30 cm`,year:2026}
+  {src:`images/36_cyanocolo.webp`,alt:`After the storm #1`,title:`Sans titre`,fr:`2026, cyanotype colorisé à la main, 42 × 29 cm`,en:`2026, hand-colored cyanotype, 42 × 29 cm`,year:2026},
+  {src:`images/36_cyanocolo2.webp`,alt:`After the storm #2`,title:`Sans titre`,fr:`2026, cyanotype colorisé à la main, 42 × 29 cm`,en:`2026, hand-colored cyanotype, 42 × 29 cm`,year:2026},
 ];
 
 function plateHTML(w){
